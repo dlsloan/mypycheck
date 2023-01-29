e2f66ea080fb5cc7dda6882750a66bf7f7ef357f
David Sloan <delus.dlsloan@gmail.com>
2023-01-29 21:50:00 +0000

    Skip already checked files

--- src/mypycheck/__init__.py
+++ src/mypycheck/__init__.py
@@ -1,9 +1,40 @@
 import os
+import sqlite3
 import subprocess as sp
 
 from pathlib import Path
 
-def check(file: str):
+db_path = Path(Path.home() / '.mypycheck.sqlite3')
+
+def _create_files_table(con: sqlite3.Connection) -> None:
+    con.execute('''CREATE TABLE IF NOT EXISTS files (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    timestamp DOUBLE NOT NULL
+                );''')
+    try:
+        con.execute('''CREATE UNIQUE INDEX idx_files_name 
+                        ON files (name);''')
+    except:
+        pass
+
+def check(file: str) -> None:
+    path = Path(file).resolve(strict=True)
+    try:
+        connection = sqlite3.connect(db_path)
+        _create_files_table(connection)
+    except:
+        if db_path.exists():
+            os.remove(db_path)
+        connection = sqlite3.connect(db_path)
+        _create_files_table(connection)
+
+    cursor = connection.execute("SELECT name,timestamp FROM files WHERE name = ?", (str(path), ))
+    row = cursor.fetchone()
+    mtime = path.stat().st_mtime
+    if row is not None and row[1] >= mtime:
+        return
+
     path = Path(file).resolve(strict=True)
     #mypy_path = sp.check_output('_pyinclude', encoding='utf-8')
     env = dict(os.environ)
@@ -11,3 +42,7 @@ def check(file: str):
         sp.check_call(['mypy', file, '--strict'])#, env=env.update({'MYPYPATH': mypy_path}))
     except sp.CalledProcessError as err:
         exit(1)
+
+    connection.execute("INSERT OR REPLACE INTO files (name, timestamp) VALUES (?, ?);", (str(path), mtime))
+    connection.commit()
+    connection.close()
