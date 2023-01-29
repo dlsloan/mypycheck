73c9424074646be044a8b8c169d1af9d9079e6ba
David Sloan <delus.dlsloan@gmail.com>
2023-01-29 20:54:39 +0000

    Drop old path includes

--- src/mypycheck/__init__.py
+++ src/mypycheck/__init__.py
@@ -5,9 +5,9 @@ from pathlib import Path
 
 def check(file: str):
     path = Path(file).resolve(strict=True)
-    mypy_path = sp.check_output('_pyinclude', encoding='utf-8')
+    #mypy_path = sp.check_output('_pyinclude', encoding='utf-8')
     env = dict(os.environ)
     try:
-        sp.check_call(['mypy', file, '--strict'], env=env.update({'MYPYPATH': mypy_path}))
+        sp.check_call(['mypy', file, '--strict'])#, env=env.update({'MYPYPATH': mypy_path}))
     except sp.CalledProcessError as err:
         exit(1)
